42b29281087d1bdf7f2cbf241ad097759b61559c
Mark Tellez <mark.tellez@icloud.com>
2017-12-26 21:06:23 -0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import React from 'react'
+import fetch from 'isomorphic-fetch'
 
-export default function App() {
-  return (
-    <div>Hello React</div>
-  )
+import Facts from './components/facts'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      facts: [],
+      newFact: '',
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  componentDidMount() {
+  }
+
+  handleChange(e) {
+    this.setState({ newFact: e.target.value })
+  }
+
+  handleSubmit() {
+  }
+
+  render() {
+    return (
+      <div>
+        <Facts
+          facts={this.state.facts}
+          newFact={this.state.newFact}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+      </div>
+    )
+  }
 }
+
+export default App
